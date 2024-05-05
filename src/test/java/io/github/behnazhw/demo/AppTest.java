76f5b9d548e168ce7a9ed20320d00bcbe9865dda
Behnaz Hassanshahi <b.hassanshahi@gmail.com>
2024-05-05 18:01:33 +1000

    build: add initial files

---
+++ src/test/java/io/github/behnazhw/demo/AppTest.java
@@ -0,0 +1,12 @@
+package io.github.behnazhw.demo;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    public void shouldAnswerWithTrue(){}
+}
